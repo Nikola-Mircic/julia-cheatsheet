a5e19762bf5ff89552c9f6a62f69e08e0b7ada76
Nikola-Mircic <nikola123mircic@gmail.com>
2022-03-12 11:18:48 +0100

    Moved sorting part to the func

--- algorithms/bubble_sort.jl
+++ algorithms/bubble_sort.jl
@@ -2,17 +2,21 @@
 # BUBBLE SORT
 ##############################
 
-# Get the array from user input
-arr = split(readline())
-
-# Sort the array
-for i = 1:length(arr), j = 1:(length(arr)-i)
-    if arr[j] > arr[j+1]
-        arr[j+1], arr[j] = arr[j], arr[j+1]
+function bubble_sort(arr)
+    for i = 1:length(arr), j = 1:(length(arr)-i)
+        if arr[j] > arr[j+1]
+            arr[j+1], arr[j] = arr[j], arr[j+1]
+        end
     end
 end
 
+# Get the array from user input
+input = split(readline())
+
+# Sort the array
+bubble_sort(input)
+
 # Print the array
-for x in arr
+for x in input
     print(x," ")
 end
